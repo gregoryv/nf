5629710f7acadc153a3a4944d36137e63b41e92c
Gregory Vincic <g@7de.se>
2021-03-08 21:04:27 +0100

    Skip if file exists

--- main.go
+++ main.go
@@ -13,34 +13,38 @@ import (
 
 func main() {
 	var (
-		cli  = cmdline.NewParser(os.Args...)
-		help = cli.Flag("-h, --help")
-
-		filename = cli.Required("FILE").String("")
+		cli       = cmdline.NewParser(os.Args...)
+		help      = cli.Flag("-h, --help")
+		_         = cli.Required("FILES...").String("")
+		filenames = cli.Args()
 	)
-
 	log.SetFlags(0)
-
 	switch {
 	case help:
 		writeUsage(os.Stdout, cli)
 		os.Exit(0)
-
 	case !cli.Ok():
 		log.Fatal(cli.Error())
-
+	case len(filenames) == 0:
+		log.Fatal("missing files")
 	}
-
-	err := writeFile(os.Stdout, filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	switch path.Ext(filename) {
-	case ".sh":
-		err := os.Chmod(filename, 0744)
+	for _, filename := range filenames {
+		if fi, _ := os.Stat(filename); fi != nil {
+			log.Println(" skip", filename)
+			continue
+		}
+		err := writeFile(os.Stdout, filename)
 		if err != nil {
 			log.Fatal(err)
 		}
+		switch path.Ext(filename) {
+		case ".sh":
+			err := os.Chmod(filename, 0744)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		log.Println("write", filename)
 	}
 }
 
